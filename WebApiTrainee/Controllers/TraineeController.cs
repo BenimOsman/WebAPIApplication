a5cdd2aed4e6dec28d38309ed6cfb3f9e0434c72
SYED FAISAL <fsyedrx2002@gmail.com>
2025-08-27 00:22:33 +0530

    Add files via upload

---
+++ WebApiTrainee/Controllers/TraineeController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiTrainee.Model;
+
+namespace WebApiTrainee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TraineeController : ControllerBase
+    {
+        private readonly TraineeContext _context;
+
+        public TraineeController(TraineeContext context)
+        {
+            _context = context;
+        }
+
+        // Get all the Trainees
+        // GET: api/Trainee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trainee>>> GetTrainees()
+        {
+            return await _context.Trainees.ToListAsync();
+        }
+
+        // Get all the Trainees by Id
+        // GET: api/Trainee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trainee>> GetTrainee(int id)
+        {
+            var trainee = await _context.Trainees.FindAsync(id);
+
+            if (trainee == null)
+            {
+                return NotFound();
+            }
+
+            return trainee;
+        }
+
+        // Post a new Trainee
+        // PUT: api/Trainee/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrainee(int id, Trainee trainee)
+        {
+            if (id != trainee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trainee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TraineeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // Post a Trainee
+        // POST: api/Trainee
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Trainee>> PostTrainee(Trainee trainee)
+        {
+            _context.Trainees.Add(trainee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTrainee", new { id = trainee.Id }, trainee);
+        }
+
+        // Delete a Trainee
+        // DELETE: api/Trainee/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrainee(int id)
+        {
+            var trainee = await _context.Trainees.FindAsync(id);
+            if (trainee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trainees.Remove(trainee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TraineeExists(int id)
+        {
+            return _context.Trainees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApiTrainee/Model/TraineeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace WebApiTrainee.Model
+{
+    public class TraineeContext : DbContext
+    {
+        public TraineeContext(DbContextOptions<TraineeContext> options) : base(options) { }
+
+        public DbSet<Trainee> Trainees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
